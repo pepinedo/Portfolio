b176f9b7e097af7a3fc689464883ffa02538245f
pedro <pedropinedocobo@hotmail.com>
2025-06-04 13:19:51 +0200

    Fix: Diseñado por Carlos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,7 @@ export const Footer = () => {
         <a href="mailto:pedropinedocobo@gmail.com">pedropinedocobo@gmail.com</a>
         <a href="tel:+34697294074">697294074</a>
         <a href="https://www.linkedin.com/in/pedro-pinedo-cobo/" target='_blank' rel="noopener noreferrer">Linkedin</a>
+        <p>Diseñado por Carlos Guerrero García</p>
         <p>Desarrollado por Pedro Pinedo Cobo</p>
     </>
   )
