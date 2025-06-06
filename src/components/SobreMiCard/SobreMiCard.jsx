270edf5ef52a3b96ac64606cd378ae9f33a87d0e
pedro <pedropinedocobo@hotmail.com>
2025-06-06 13:04:44 +0200

    Cambios menores

--- src/components/SobreMiCard/SobreMiCard.jsx
+++ src/components/SobreMiCard/SobreMiCard.jsx
@@ -1,21 +1,30 @@
 import "./sobreMiCard.css"
+import { motion } from "framer-motion"
 
 export const SobreMiCard = ({elem}) => {
   return (
-        <article className="sobreMiCard" key={elem.index}>
-            <img className="sobreMiCard__foto" src={elem.foto} alt="" />
-            <div className="sobreMiCard__texto">
-                <h1>{elem.titulo} </h1>
-                {elem.descripcion.map((p)=>{
-                    return(
-                        <>
-                            <br />
-                            <p>{p}</p>
-                        </>
-                    )
-                })}
-            </div>
-            <div className="sobreMiCard__LineaAbajo"></div>
-        </article>
+        <motion.div
+            initial={{ opacity: 0, scale: 0.90 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            viewport={{ once: true, margin: "0px 0px -50px 0px" }}
+            transition={{ duration: 0.5, ease: "easeOut", delay: 0.1 }}
+            className="card"
+        >
+            <article className="sobreMiCard" key={elem.index}>
+                <img className="sobreMiCard__foto" src={elem.foto} alt="" />
+                <div className="sobreMiCard__texto">
+                    <h1>{elem.titulo} </h1>
+                    {elem.descripcion.map((p)=>{
+                        return(
+                            <>
+                                <br />
+                                <p>{p}</p>
+                            </>
+                        )
+                    })}
+                </div>
+                <div className="sobreMiCard__LineaAbajo"></div>
+            </article>
+        </motion.div>
   )
 }
--- src/data/sobreMi.js
+++ src/data/sobreMi.js
@@ -2,7 +2,7 @@ export const sobreMi = [
     {
         titulo: "La escalada",
         descripcion: [
-            'Hace 4 años descubrí la escalada, en concreto la "escalada deportiva". En ese momento iba al gimnasio y siempre que todaba la hora de ir hacía un enorme esfuerzo. ¿Porqué? Pues porque no me gustaba, iba por estar sano y hacer ejercicio, pero era un suplicio. Hasta que un día un amigo me avisó para ir con el a la montaña y descubrí la escalada. Al principio era muy reticente, puesto que tengo vértigo a las alturas, pero subí esos 35 metros.',
+            'Hace 4 años descubrí la "escalada deportiva". En ese momento iba al gimnasio y siempre que todaba la hora de ir hacía un enorme esfuerzo. ¿Porqué? Pues porque no me gustaba, iba por estar sano y hacer ejercicio, pero era un suplicio, hasta que un día un amigo me avisó para ir con el a la montaña. Al principio era muy reticente, puesto que tengo vértigo a las alturas, pero subí esos 35 metros.',
             'A partir de ese día pensé: ¿Y porqué no voy al rocódromo en vez de al gimnasio? O sea, aparte de hacer deporte, me lo paso bien. Y desde entonces esa es mi rutina, ir al rocódromo entre semana y los fines de semana que puedo ir a la montaña.'
         ],
         foto: "images/sobreMi/escalada.jpeg"
@@ -10,10 +10,20 @@ export const sobreMi = [
         {
         titulo: "Arwen",
         descripcion: [
-            'Te presento a Arwen, mi sobrina. Hace muy poquito mi hermana adquirió a su pequeña hija. Es monísima ¿A que sí? Y encima es buenisima. ',
-            'Es la alegría de la familia, siempre que te ve se pone contentísima y quiere que le hagas todo tipo de mimitos, es que es para comersela.',
-            'Y por si te lo preguntas, si, su nombre viene de "El señor de los anillos", gran saga.'
+            'Te presento a Arwen, mi sobrina. Hace muy poquito mi hermana adquirió a su pequeña hija. Es monísima ¿A que sí? Y encima es buenisima, se deja hacer de todo, es un peluchito. ',
+            'Es la alegría de la familia, siempre que te ve se pone contentísima y quiere que le hagas todo tipo de mimitos, de hecho, el único momento en el que ladra es cuando te ve, que se pone super contenta.',
+            'Uno de sus momentos más adorables es cuando se queda dormida. Se acurruca como un ovillito, con sus patitas bajo el ocico.'
         ],
         foto: "images/sobreMi/Arwen.jpeg"
+    },
+    {
+        titulo: "Juegos de mesa",
+        descripcion: [
+            'Otro de mis grandes hobbies son los juegos de mesa. Grandes, chicos, largos, cortos, de cartas, de miniaturas, de rol,... Lo que me gusta es jugar y pasar el rato con los amigos. ',
+            'Lo que más valoro de esto es la diversidad. Puedes pasar de un juego cooperativo donde todos luchan con un objetivo común, a un juego competitivo, donde todos lucháis para ver quién se hace con la victoria.',
+            'Aún así, si que quiero destacar Magic The Gathering. Llevo jugando Magic desde que tengo conciencia, minimo harán ya 20 años, con épocas de jugar más y de jugar menos, por su puesto.',
+            '',
+        ],
+        foto: "images/sobreMi/juegosDeMesa1.jpeg"
     },
 ]
